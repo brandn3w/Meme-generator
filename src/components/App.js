40c0f0deaed9931ff6a066164a1e1f797686ff64
Nadja Brandys <nadiusza@gmail.com>
2020-05-17 22:20:42 +0200

    app ready, one bug to be fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import ReactDOM from "react-dom"
+
 import MemeGenerator from "./MemeGenerator"
 import Header from "./Header"
 
--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { render } from "enzyme"
+
 
 
 class MemeGenerator extends React.Component(){
@@ -12,6 +12,7 @@ class MemeGenerator extends React.Component(){
             allmemeImgs:[]
         }
         this.handleChange=this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 componentDidMount(){
     fetch("https://api.imgflip.com/get_memes")
@@ -26,14 +27,26 @@ handleChange(event){
 const {name, value}= event.target
 this.setState({[name]: value})
 }
+
+handleSubmit(event){
+    event.preventDefault()
+    const randNum = Math.floor(Math.random()*this.state.allmemeImgs.length)
+    const randMemeImg = this.state.allmemeImgs[randNum].url
+    this.setState({randomImg: randMemeImg})
+}
 render(){
     return(
         <div>
-        <form className="mame=form">
-<input type="text" name="topText" value={this.state.topText} placeholder="Top Text" onChange={}/>
-<input type="text" name="bottomText" value={this.state.bottomText} placeholder="Bottom Text" onChange={}/>
-
+        <form className="meme=form" onSubmit={this.handleSubmit}>
+<input type="text" name="topText" value={this.state.topText} placeholder="Top Text" onChange={this.handleChange}/>
+<input type="text" name="bottomText" value={this.state.bottomText} placeholder="Bottom Text" onChange={this.handleChange}/>
+<button>Gen</button>
         </form>
+        <div className="meme">
+            <img src={this.state.randomImg} alt=""/>
+            <h2 className="top">{this.state.topText}</h2>
+            <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
         </div>
     )
 }
